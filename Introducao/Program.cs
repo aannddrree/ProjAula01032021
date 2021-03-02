ef45daca493d5738f6f23ff25882ee1b6910f302
unknown <aannddrree>
2021-03-01 21:18:35 -0300

    implantacao

---
+++ Introducao/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introducao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome = "André";
+            Console.WriteLine("Ex1 - Nome: " + nome);
+
+            int a = 2;
+            int b = 3;
+            int c = a + b;
+
+            Console.WriteLine("Ex2 - Soma de A + B: " + c);
+            Console.WriteLine("Ex3 - Soma de A + B: " + a + b);
+            Console.WriteLine("Ex4 - Soma de A + B: " + (a + b));
+
+            string texto = "Ex5 - O meu nome é " + nome + "\nEu tenho " + c + " livros";
+            Console.WriteLine(texto);
+
+            bool isValid = true; 
+            bool isNotValid = false;
+
+            Console.WriteLine("Ex6 - isValid: " + isValid + "\nisNotValid:" + isNotValid);
+
+            char letra = nome[3];
+            Console.WriteLine("Ex7 - Nome[3]: " + letra);
+
+            //Console.WriteLine("Hello World!");
+
+            // Comentário uma linha
+
+            /*
+             * Comentário Bloco
+             * 
+             */
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Introducao/Requirement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introducao
+{
+    class Requirement
+    {
+        static void Main (string[] args)
+        {
+            Console.Write("Idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 18) { 
+                Console.WriteLine("Maior de Idade");
+                Console.WriteLine("A idade digitada é: " + idade);
+            }
+            else
+                Console.WriteLine("Menor de Idade");
+
+            switch (idade)
+            {
+                case 10:
+                    Console.WriteLine("10");
+                    break;   
+                default:
+                    Console.WriteLine("default");
+                    break;
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
